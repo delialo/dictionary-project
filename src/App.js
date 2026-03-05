96a457e7be6634b4acc8319ffa18e8f9fa8cda08
delialo <yuu.miyamichi@gmail.com>
2026-03-05 15:42:10 +0100

    initial css adjustments

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import Dictionary from "./Dictionary";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <main className="container m-5">
+    <div className="App text-center">
+      <header className="App-header m-5">
+        <h1>Dictionary App</h1>
+      </header>
+      <main className="container">
         <Dictionary />
       </main>
-      <footer className="text-center m-3">
-        <small>Open sourced on GitHub and hosted on Netlify</small>
+      <footer className="App-footer m-5">
+        <small>
+          Open sourced on{" "}
+          <a
+            href="https://github.com/delialo/dictionary-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://ld-dictionary.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Netlify
+          </a>
+        </small>
       </footer>
     </div>
   );
