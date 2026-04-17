befc11bf49afebf2a176c98be97e3dfd7e7f142d
delialo <yuu.miyamichi@gmail.com>
2026-04-17 16:45:59 +0200

    added phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,21 +1,25 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
-  if (props.results) {
-    return (
-      <div className="Results">
-        <h1 className="m-5 fw-bold">{props.results.word}</h1>
-        {props.results.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
-          );
-        })}
-      </div>
-    );
-  } else {
+  if (!props.results) {
     return null;
   }
+
+  return (
+    <div className="Results">
+      <h1 className="m-5 fw-bold">{props.results.word}</h1>
+      <strong>Phonetics: </strong>
+      {props.results.phonetic && <Phonetic phonetic={props.results.phonetic} />}
+
+      {props.results.meanings.map(function (meaning, index) {
+        return (
+          <div key={index}>
+            <Meaning meaning={meaning} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
